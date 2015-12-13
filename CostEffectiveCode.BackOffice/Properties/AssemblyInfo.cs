1e62eedc67d8dc45050b7fa2c265b84ee136a94e
Andrey Pesoshin <pesoshin@gmail.com>
2015-12-13 12:08:32 +0300

    fixed version, provided assembly description

--- CostEffectiveCode.BackOffice/Properties/AssemblyInfo.cs
+++ CostEffectiveCode.BackOffice/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CostEffectiveCode.BackOffice")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helps you automate building standard back-office area using SPA approach with Backend implemented as WebApi. Contains reusable components, such as generic WebApi2-controller, and abstract infrastructure for WebApi pipeline.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HighTech")]
 [assembly: AssemblyProduct("CostEffectiveCode.BackOffice")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
